60a9b32cfd3bccee651cbd243f1c6a0a77b5873c
codeman1o1 <70335544+codeman1o1@users.noreply.github.com>
2023-09-18 21:05:56 +0200

    feat: 📱 Add mobile support

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen">
     <h1 class="m-5 text-5xl text-center">codeman1o1.me</h1>
-    <div class="grid grid-cols-6 gap-4 p-4">
-      <a
-        v-for="app in apps"
-        :key="app.name"
-        :href="app.url"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="flex flex-col items-center p-4 hover:bg-gray-300 transition-colors rounded-md"
-      >
-        <img
-          :src="app.imagePath ?? `/images/${app.name}.svg`"
-          :alt="`${app.name} logo`"
-          class="w-32 h-32"
-        />
-        <p class="mt-2 text-xl text-center parent">{{ app.name }}</p>
-      </a>
+    <div class="flex-grow">
+      <div class="grid gap-4 p-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6">
+        <a
+          v-for="app in apps"
+          :key="app.name"
+          :href="app.url"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="flex flex-col items-center p-4 hover:bg-gray-300 transition-colors rounded-md"
+        >
+          <img
+            :src="app.imagePath ?? `/images/${app.name}.svg`"
+            :alt="`${app.name} logo`"
+            class="w-32 h-32"
+          />
+          <p class="mt-2 text-xl text-center parent">{{ app.name }}</p>
+        </a>
+      </div>
     </div>
-    <footer class="fixed bottom-0 left-0 w-full bg-gray-400 text-center p-2">
+    <footer class="w-full bg-gray-400 text-center p-2">
       <p>
         Made with
         <a href="https://bun.sh" rel="noopener noreferrer" target="_blank">Bun</a>,
