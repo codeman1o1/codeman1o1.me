e865d084dc205afff4d777451bc61cdf737dd9bf
codeman1o1 <70335544+codeman1o1@users.noreply.github.com>
2023-10-01 19:28:49 +0200

    feat: ✨ Add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,20 @@ import { fileURLToPath, URL } from "node:url"
 
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "codeman1o1.me",
+        short_name: "codeman1o1.me",
+        icons: [{ src: "/icon-144x144.png", sizes: "144x144", type: "image/png" }]
+      }
+    })
   ],
   resolve: {
     alias: {
